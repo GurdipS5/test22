e6df43b87ab4e67545046df462bb285e5c1c04f3
Gurdip Sira <gurdip.sira@gmail.com>
2022-12-19 16:23:32 +0000

    de

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using Pulumi;
 
 return await Deployment.RunAsync(() =>
 {
-   // Add your resources here
+   // Add your resources here ssdffds
    // e.g. var resource = new Resource("name", new ResourceArgs { });
 
    // Export outputs here
